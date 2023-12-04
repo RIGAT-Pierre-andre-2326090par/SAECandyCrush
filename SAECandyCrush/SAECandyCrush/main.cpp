08433f754a7cc54fc328c5398c3dd0ac36db7fdf
RIGAT Pierre-André <145963275+RIGAT-Pierre-andre-2326090par@users.noreply.github.com>
2023-12-04 16:15:23 +0100

    update le main.cpp avec celui du prof

--- SAECandyCrush/SAECandyCrush/main.cpp
+++ SAECandyCrush/SAECandyCrush/main.cpp
@@ -1,9 +1,80 @@
 #include <iostream>
+#include <vector>
+#include <iomanip>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+//***********************************************************************************/
+//***********************    R1.01 – Prog#10 Exercice 1   ***************************/
+//***********************************************************************************/
+
+
+void clearScreen () {
+    cout << "\033[H\033[2J";
 }
+
+const unsigned KReset   (0);
+const unsigned KNoir    (30);
+const unsigned KRouge   (31);
+const unsigned KVert    (32);
+const unsigned KJaune   (33);
+const unsigned KBleu    (34);
+const unsigned KMAgenta (35);
+const unsigned KCyan    (36);
+const unsigned KBGNoir    (40);
+const unsigned KBGRouge   (41);
+const unsigned KGBBleu    (44);
+const unsigned KBGCyan    (46);
+
+void couleur (const unsigned & coul) {
+    cout << "\033[" << coul <<"m";
+}
+
+
+//***********************************************************************************/
+//***********************    R1.01 – Prog#10 Exercice 2   ***************************/
+//***********************************************************************************/
+/*
+ *
+*/
+
+typedef ushort contenueDUneCase;
+typedef vector <contenueDUneCase> CVLigne; // un type représentant une ligne de la grille
+typedef vector <CVLigne> CMatrice; // un type représentant la grille
+
+const contenueDUneCase KAIgnorer = 0;
+const contenueDUneCase KPlusGrandNombreDansLaMatrice = 4;
+
+//initialisation de la grille de jeu
+void initMat (CMatrice & mat, const size_t & nbLignes = 10,
+             const size_t & nbColonnes = 10,
+              const unsigned & nbMax= KPlusGrandNombreDansLaMatrice){
+//TODO
+}
+
+// affichage de la matrice sans les numéros de lignes / colonnes en haut / à gauche
+void  afficheMatriceV0 (const CMatrice & Mat) {
+    //TODO
+}
+
+// affichage de la matrice sans les numéros de lignes / colonnes en haut / à gauche, mais avec un fond de couleur
+//pour signifier que la case est a KAIgnorer
+void  afficheMatriceV1 (const CMatrice & Mat) {
+    //TODO
+}
+
+
+// affichage de la matrice avec les numéros de lignes / colonnes en haut / à gauche et avec un fond de couleur
+//pour signifier que la case est a KAIgnorer
+void  afficheMatriceV2 (const CMatrice & mat) {
+    //TODO
+}
+
+
+//***********************************************************************************/
+//***********************    R1.01 – Prog#10 Exercice 2   ***************************/
+//***********************************************************************************/
+
+
+// on remplira cela plus tard, juste la définition de la fonction
+void explositionUneBombeHorizontale (CMatrice & mat, const size_t & numLigne,
