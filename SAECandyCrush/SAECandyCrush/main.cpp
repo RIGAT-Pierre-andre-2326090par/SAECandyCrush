02b7d7b65b66f972fc4da8735255bf9374dcdbf2
RIGAT Pierre-André <145963275+RIGAT-Pierre-andre-2326090par@users.noreply.github.com>
2023-12-04 16:19:06 +0100

    Update main.cpp

--- SAECandyCrush/SAECandyCrush/main.cpp
+++ SAECandyCrush/SAECandyCrush/main.cpp
@@ -48,8 +48,8 @@ const contenueDUneCase KPlusGrandNombreDansLaMatrice = 4;
 //initialisation de la grille de jeu
 void initMat (CMatrice & mat, const size_t & nbLignes = 10,
              const size_t & nbColonnes = 10,
-              const unsigned & nbMax= KPlusGrandNombreDansLaMatrice){
-//TODO
+             const unsigned & nbMax= KPlusGrandNombreDansLaMatrice){
+    //TODO
 }
 
 // affichage de la matrice sans les numéros de lignes / colonnes en haut / à gauche
@@ -78,3 +78,142 @@ void  afficheMatriceV2 (const CMatrice & mat) {
 
 // on remplira cela plus tard, juste la définition de la fonction
 void explositionUneBombeHorizontale (CMatrice & mat, const size_t & numLigne,
+                                    const size_t & numColonne, const size_t & combien);
+
+
+//
+bool detectionExplositionUneBombeHorizontale (CMatrice & mat){
+    bool auMoinsUneExplosion (false);
+
+    //on parcours la matrice case / case
+    // si on tombe sur la valeur KAIgnorer, on passe a la case suivante
+    // sinon on compte combien de fois on a la même valeur
+    size_t combienALaSuite (1);
+    //si on a aun moins 3 chiffres identiques a la suite
+    if (combienALaSuite >= 3){
+        auMoinsUneExplosion = true;
+        cout << "on a une suite en position numLigne = " << numLigne
+             << "; colonne = " << numCol
+             << "; sur  " << combienALaSuite << " cases" << endl;
+        cout << string (20, '-') << endl << "matrice avant suppresion" << endl;
+        afficheMatriceV1(mat);
+        /*            explositionUneBombeHorizontale (mat, numLigne, numCol, combienALaSuite);
+                cout << string (20, '-') << endl << "matrice après suppresion" << endl;
+                afficheMatriceV1(mat);*/
+    }
+}
+}
+return auMoinsUneExplosion;
+}
+
+//***********************************************************************************/
+//***********************    R1.01 – Prog#10 Exercice 3   ***************************/
+//***********************************************************************************/
+
+
+//fait descendre toutes les cases d'une unité suite à une explosition
+void explositionUneBombeHorizontale (CMatrice & mat, const size_t & numLigne,
+                                    const size_t & numColonne, const size_t & combien){
+}
+
+void faitUnMouvement (CMatrice & mat, const char & deplacment, const size_t & numLigne,
+                     const size_t & numCol) {
+
+    size_t nouvellePositionLigne (numLigne), nouvellePositionColonne (numCol);
+    switch (tolower(deplacment)) {
+    case 'a':
+        //TODO
+        break;
+    case 'z':
+        //TODO
+        break;
+        //TODO : completer les autres mouvements
+    default:
+        cout<<"Tu choisis A ou Z ou E  ou Q ou D ou X ou C ou V"<<endl;
+        break;
+    }
+    //faire la permutaion entre les 2 cases
+}
+
+
+int ppalExo01 (){
+    CMatrice mat;
+    initMat(mat);
+    // affichage de la matrice sans les numéros de lignes / colonnes en haut / à gauche
+    afficheMatriceV0 (mat);
+    //detectionExplositionUneBombeHorizontale (mat);
+    return 0;
+}
+
+int ppalExo02 (){
+    CMatrice mat;
+    initMat(mat);
+    // affichage de la matrice sans les numéros de lignes / colonnes en haut / à gauche
+    afficheMatriceV1 (mat);
+    detectionExplositionUneBombeHorizontale (mat);
+    afficheMatriceV1 (mat);
+    return 0;
+}
+
+int ppalExo03 (){
+    CMatrice mat;
+    initMat(mat);
+    // affichage de la matrice sans les numéros de lignes / colonnes en haut / à gauche
+    afficheMatriceV1 (mat);
+    while (detectionExplositionUneBombeHorizontale (mat)) {
+        mat[5][8] = 4;
+        afficheMatriceV1 (mat);
+    }
+    return 0;
+}
+
+int ppalExo04 (){
+    CMatrice mat;
+    initMat(mat);
+    // affichage de la matrice sans les numéros de lignes / colonnes en haut / à gauche
+    detectionExplositionUneBombeHorizontale (mat);
+    afficheMatriceV2 (mat);
+    //condition de victoire a trouver
+    while (true) {
+        cout << "Fait un mouvement ";
+        cout << "numero de ligne : ";
+        size_t numLigne;
+        cout << numLigne;
+        cout << "numero de colonne : ";
+        size_t numCol;
+        cout << numCol;
+        cout << "Sens du deplacement : (A|Z|E|Q|D|W|X|C) : " << endl;
+        char deplacement;
+        cin >> deplacement;
+        faitUnMouvement (mat, deplacement, numLigne, numCol);
+        detectionExplositionUneBombeHorizontale (mat);
+        afficheMatriceV2 (mat);
+    }
+    return 0;
+}
+
+
+int main() {
+
+    // ---------Exercice 2 -----------------//
+    //    clearScreen();
+
+    //    CMatrix mat (10, CVLine (10, kEmpty));
+    //    mat [0][mat.size()-1] = kTokenPlayer1;
+    //    mat [mat.size()-1][0] = kTokenPlayer2;
+    //    showMatrix(mat);
+    //-------------------------------------//
+    //return 0;
+
+    // ---------Exercice 2 -----------------//
+    //return ppalExo02();
+    //-------------------------------------//
+
+    // ---------Exercice 3 -----------------//
+    //return ppalExo03();
+    //-------------------------------------//
+
+    // ---------Exercice 3 -----------------//
+    return ppalExo04();
+    //-------------------------------------//
+}
