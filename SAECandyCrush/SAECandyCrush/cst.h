48854549097118f1ef775eddfee5503a506abcfa
Cyril <146132293+CyrilFT@users.noreply.github.com>
2023-12-12 10:57:34 +0100

    Add files via upload

---
+++ SAECandyCrush/SAECandyCrush/cst.h
@@ -0,0 +1,21 @@
+#include <type.h>
+#ifndef CST_H
+#define CST_H
+const unsigned KReset   (0);
+const unsigned KNoir    (30);
+const unsigned KRouge   (31);
+const unsigned KVert    (32);
+const unsigned KJaune   (33);
+const unsigned KBleu    (34);
+const unsigned KMagenta (35);
+const unsigned KCyan    (36);
+const unsigned KBGNoir    (40);
+const unsigned KBGRouge   (41);
+const unsigned KGBBleu    (44);
+const unsigned KBGCyan    (46);
+const unsigned KBGGrisClair(47);
+
+const contenueDUneCase KAIgnorer = 0;
+const contenueDUneCase KPlusGrandNombreDansLaMatrice = 4;
+
+#endif // CST_H
---
+++ SAECandyCrush/SAECandyCrush/type.h
@@ -0,0 +1,14 @@
+#include <vector>
+#ifndef TYPE_H
+#define TYPE_H
+using namespace std;
+
+typedef vector <char> CVLine; // un type représentant une ligne de la grille
+typedef vector <CVLine> CMatrix; // un type représentant la grille
+typedef pair   <unsigned, unsigned> CPosition; // un type représentant une coordonnée dans la grille
+
+typedef unsigned short contenueDUneCase;
+typedef vector <contenueDUneCase> CVLigne; // un type représentant une ligne de la grille
+typedef vector <CVLigne> CMatrice; // un type représentant la grille
+
+#endif // TYPE_H
