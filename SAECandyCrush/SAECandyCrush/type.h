334e5852d712cd13b6b404abc1aafcd0d6f6159f
RIGAT Pierre-André <145963275+RIGAT-Pierre-andre-2326090par@users.noreply.github.com>
2023-12-16 16:54:39 +0100

    Update type.h

--- SAECandyCrush/SAECandyCrush/type.h
+++ SAECandyCrush/SAECandyCrush/type.h
@@ -1,14 +1,13 @@
 #include <vector>
 #ifndef TYPE_H
 #define TYPE_H
-using namespace std;
 
-typedef vector <char> CVLine; // un type représentant une ligne de la grille
-typedef vector <CVLine> CMatrix; // un type représentant la grille
-typedef pair   <unsigned, unsigned> CPosition; // un type représentant une coordonnée dans la grille
+typedef std::vector <char> CVLine; // un type représentant une ligne de la grille
+typedef std::vector <CVLine> CMatrix; // un type représentant la grille
+typedef std::pair   <unsigned, unsigned> CPosition; // un type représentant une coordonnée dans la grille
 
 typedef unsigned short contenueDUneCase;
-typedef vector <contenueDUneCase> CVLigne; // un type représentant une ligne de la grille
-typedef vector <CVLigne> CMatrice; // un type représentant la grille
+typedef std::vector <contenueDUneCase> CVLigne; // un type représentant une ligne de la grille
+typedef std::vector <CVLigne> CMatrice; // un type représentant la grille
 
 #endif // TYPE_H
