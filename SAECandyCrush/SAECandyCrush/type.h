2a26a4e5d13588d9bfe2d9b9375cf81370484cfd
Cyril <146132293+CyrilFT@users.noreply.github.com>
2023-12-18 14:14:55 +0100

    Update type.h

--- SAECandyCrush/SAECandyCrush/type.h
+++ SAECandyCrush/SAECandyCrush/type.h
@@ -9,5 +9,14 @@ typedef std::pair   <unsigned, unsigned> CPosition; // un type représentant une
 typedef unsigned short contenueDUneCase;
 typedef std::vector <contenueDUneCase> CVLigne; // un type représentant une ligne de la grille
 typedef std::vector <CVLigne> CMatrice; // un type représentant la grille
+struct position{
+    std::size_t _ligne;
+    std::size_t _colonne;
+};
 
+#include <map>
+struct CMyParam {
+std::map <std::string, char> mapParamChar;
+std::map <std::string, unsigned> mapParamUnsigned;
+};
 #endif // TYPE_H
