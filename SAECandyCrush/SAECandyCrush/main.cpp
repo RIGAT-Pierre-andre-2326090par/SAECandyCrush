24e65f23bb300c09507b7a2f945b28c7b4f5c634
Arnaud Nurdin <146205767+NURDIN-Arnaud-2326082b@users.noreply.github.com>
2023-12-19 11:09:39 +0100

    mode de jeu

--- SAECandyCrush/SAECandyCrush/main.cpp
+++ SAECandyCrush/SAECandyCrush/main.cpp
@@ -406,8 +406,32 @@ int main() {
 
     unsigned score=0;
     unsigned nbDeplacement=15;
+    unsigned mode ;
+    cout << " Sur quel mode de jeu voulez vous jouer ? :" << endl << endl << "1 : mode avec MinGl" << '\t' << "2 : mode avec les nombres" << endl ;
+    cin >> mode ;
+    bool test = false ;
+    if (mode ==1 || mode==2)
+    {
+        test = true ;
+    }
+    if (test == false)
+    {
+        while (test == false)
+        {
+            cout << "Ce mode de jeu n'existe pas rééssayez !" << endl << endl <<" Sur quel mode de jeu voulez vous jouer ? :" << endl << endl << "1 : mode avec MinGl" << '\t' << "2 : mode avec les nombres" << endl ;
+            cin >> mode ;
+            if (mode ==1 || mode==2)
+            {
+                test = true ;
+            }
+        }
+    }
+    if (mode == 1)
+    {
 
+    }
+    else if (mode == 2)
+    {
     return partiCasaliCrush(score, nbDeplacement);
-
-    //return 0;
+    }
 }
