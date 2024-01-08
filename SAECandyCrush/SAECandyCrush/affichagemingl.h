fede7d63748d01083768c0090da8f236e7b52173
Cyril <146132293+CyrilFT@users.noreply.github.com>
2024-01-08 10:46:19 +0100

    Update affichagemingl.h

--- SAECandyCrush/SAECandyCrush/affichagemingl.h
+++ SAECandyCrush/SAECandyCrush/affichagemingl.h
@@ -8,17 +8,17 @@
 #include <mingl/graphics/idrawable.h>
 
 /**
- * @brief affichage de la matrice avec des cercles de couleurs différentes, représentant le contenu des cases
- * @param Mat: matrice gérer par une fonction appelé par la fonction main
- * @param window: fenêtre active où le texte va être affiché
+ * @brief Affichage de la matrice avec des cercles de couleurs différentes, représentant le contenu des cases
+ * @param Mat: matrice gérée par une fonction appelé par la fonction main
+ * @param window: fenêtre active où le texte va être affichée
  * @authors A.Nurdin, A.Goncalves, P-A.Rigat
  */
 void  afficheMatriceMingl (const CMatrice & Mat , MinGL & window);
 
 /**
- * @brief dessine un rectangle de couleur bleu avec comme origine x, y
- * @param window: fenêtre active où la forme va être dessiné
- * @param x: absisse de l'origine du rectangle
+ * @brief dessine un rectangle de couleur bleue avec comme origine x, y
+ * @param window: fenêtre active où la forme va être dessinée
+ * @param x: abscisse de l'origine du rectangle
  * @param y: ordonnée de l'origine du rectangle
  * @author P-A.Rigat
  */
@@ -26,8 +26,8 @@ void dessinerRectangle (MinGL & window, const unsigned & x, const unsigned & y);
 
 /**
  * @brief dessine un cercle de couleur rouge avec comme origine x, y
- * @param window: fenêtre active où la forme va être dessiné
- * @param x: absisse de l'origine du cercle
+ * @param window: fenêtre active où la forme va être dessinée
+ * @param x: abscisse de l'origine du cercle
  * @param y: ordonnée de l'origine du cercle
  * @author P-A.Rigat
  */
@@ -35,8 +35,8 @@ void dessinerCercle (MinGL & window, const unsigned & x, const unsigned & y);
 
 /**
  * @brief dessine un triangle de couleur verte avec comme origine x, y
- * @param window: fenêtre active où la forme va être dessiné
- * @param x: absisse de l'origine du triangle
+ * @param window: fenêtre active où la forme va être dessinée
+ * @param x: abscisse de l'origine du triangle
  * @param y: ordonnée de l'origine du triangle
  * @author P-A.Rigat
  */
@@ -44,17 +44,17 @@ void dessinerTriangle (MinGL & window, const unsigned & x, const unsigned & y);
 
 /**
  * @brief dessine une croix de couleur jaune avec comme origine x, y
- * @param window: fenêtre active où la forme va être dessiné
- * @param x: absisse de l'origine de la croix
+ * @param window: fenêtre active où la forme va être dessinée
+ * @param x: abscisse de l'origine de la croix
  * @param y: ordonnée de l'origine de la croix
  * @author P-A.Rigat
  */
 void dessinerCroix (MinGL & window, const unsigned & x, const unsigned & y);
 
 /**
- * @brief dessine un curseur(composé de 4 triangles) de couleur blanche avec comme origine x, y
- * @param window: fenêtre active où la forme va être dessiné
- * @param x: absisse de l'origine du curseur
+ * @brief dessine un curseur (composé de 4 triangles) de couleur blanche avec comme origine x, y
+ * @param window: fenêtre active où la forme va être dessinée
+ * @param x: abscisse de l'origine du curseur
  * @param y: ordonnée de l'origine du curseur
  * @author P-A.Rigat
  */
@@ -62,8 +62,8 @@ void dessinerCurseur (MinGL & window, const unsigned & x, const unsigned & y);
 
 /**
  * @brief dessine une étoile de couleur violette avec comme origine x, y
- * @param window: fenêtre active où la forme va être dessiné
- * @param x: absisse de l'origine de l'étoile
+ * @param window: fenêtre active où la forme va être dessinée
+ * @param x: abcsisse de l'origine de l'étoile
  * @param y: ordonnée de l'origine de l'étoile
  * @author P-A.Rigat
  */
@@ -71,9 +71,9 @@ void dessinerEtoile (MinGL & window, const unsigned & x, const unsigned & y);
 
 /**
  * @brief affiche le texte txt en blanc au coordonné x, y
- * @param window: fenêtre active où le texte va être affiché
+ * @param window: fenêtre active où le texte va être affichée
  * @param txt: texte à afficher
- * @param x: absisse de l'origine du texte
+ * @param x: abscisse de l'origine du texte
  * @param y: ordonnée de l'origine du texte
  * @author P-A.Rigat
  */
@@ -81,12 +81,12 @@ void afficheText (MinGL & window, const std::string & txt, const unsigned & x, c
 
 /**
  * @brief permet de déplacer le curseur dans la fenêtre mingl
- * @param mat: matrice gérer par une fonction appelé par la fonction main
- * @param numLigne: absisse du curseur
+ * @param mat: matrice gérée par une fonction appelée par la fonction main
+ * @param numLigne: abscisse du curseur
  * @param numCol: ordonnée du curseur
- * @param nbDeplacement: nombre de déplacement restant, gérer par la fonction main
- * @param param: structure de variable gérer par la fonction main
- * @param curs2: booléen permettant de savoir si il y a une case sélectionné ou non
+ * @param nbDeplacement: nombre de déplacements restants, géré par la fonction main
+ * @param param: structure de variable géré par la fonction main
+ * @param curs2: booléen permettant de savoir s'il y a une case sélectionnée ou non
  * @bug certaines détections sont mal placées
  * @authors A.Casali, P-A.Rigat
  */
@@ -96,8 +96,8 @@ void faitUnMouvementMinGL (CMatrice & mat, MinGL & window, size_t & numLigne,
 
 /**
  * @brief affiche la tête de Baptiste avec comme origine x, y
- * @param window: fenêtre active où la forme va être dessiné
- * @param x: absisse de l'origine de l'image
+ * @param window: fenêtre active où la forme va être dessinée
+ * @param x: abscisse de l'origine de l'image
  * @param y: ordonnée de l'origine de l'image
  * @author P-A.Rigat
  */
@@ -105,8 +105,8 @@ void dessineBaptiste (MinGL & window, const unsigned & x, const unsigned & y);
 
 /**
  * @brief affiche la tête d'Alex avec comme origine x, y
- * @param window: fenêtre active où la forme va être dessiné
- * @param x: absisse de l'origine de l'image
+ * @param window: fenêtre active où la forme va être dessinée
+ * @param x: abscisse de l'origine de l'image
  * @param y: ordonnée de l'origine de l'image
  * @author P-A.Rigat
  */
@@ -114,8 +114,8 @@ void dessineAlex (MinGL & window, const unsigned & x, const unsigned & y);
 
 /**
  * @brief affiche la tête de Pierre-André avec comme origine x, y
- * @param window: fenêtre active où la forme va être dessiné
- * @param x: absisse de l'origine de l'image
+ * @param window: fenêtre active où la forme va être dessinée
+ * @param x: abscisse de l'origine de l'image
  * @param y: ordonnée de l'origine de l'image
  * @author P-A.Rigat
  */
@@ -123,8 +123,8 @@ void dessinePierre (MinGL & window, const unsigned & x, const unsigned & y);
 
 /**
  * @brief affiche la tête de Cyril avec comme origine x, y
- * @param window: fenêtre active où la forme va être dessiné
- * @param x: absisse de l'origine de l'image
+ * @param window: fenêtre active où la forme va être dessinée
+ * @param x: abscisse de l'origine de l'image
  * @param y: ordonnée de l'origine de l'image
  * @author P-A.Rigat
  */
@@ -132,8 +132,8 @@ void dessineCyril (MinGL & window, const unsigned & x, const unsigned & y);
 
 /**
  * @brief affiche la tête d'Arnaud avec comme origine x, y
- * @param window: fenêtre active où la forme va être dessiné
- * @param x: absisse de l'origine de l'image
+ * @param window: fenêtre active où la forme va être dessinée
+ * @param x: abscisse de l'origine de l'image
  * @param y: ordonnée de l'origine de l'image
  * @author P-A.Rigat
  */
@@ -141,8 +141,8 @@ void dessineArnaud (MinGL & window, const unsigned & x, const unsigned & y);
 
 /**
  * @brief affiche la tête de A.Casali avec comme origine x, y
- * @param window: fenêtre active où la forme va être dessiné
- * @param x: absisse de l'origine de l'image
+ * @param window: fenêtre active où la forme va être dessinée
+ * @param x: abscisse de l'origine de l'image
  * @param y: ordonnée de l'origine de l'image
  * @author P-A.Rigat
  */
